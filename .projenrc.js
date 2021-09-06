9f3f179b652470d2175fc0fc974cb9664cd5e65b
Wayne Gibson <wayne.gibson@spacecomx.com>
2021-09-06 05:50:14 -0400

    chore(projenrc): updated packages adn bumped cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -10,7 +10,7 @@ const RELEASE_BRANCH = 'main';
 const PRE_RELEASE = '';
 const MAYOR_VERSION = 1;
 const RELEASE_TO_NPM = true;
-const AWS_CDK_VERSION = '1.119.0';
+const AWS_CDK_VERSION = '1.121.0';
 const AUTOMATION_TOKEN = 'GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
